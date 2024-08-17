fb7d27019ca1ab3c1f8e762985dcb499a4688533
Suryansh <suryanshvaish1@gmail.com>
2024-08-18 00:10:35 +0530

    Updating url

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        banner: "url(./src/assets/banner.jpg)"
+        banner: "url(https://img.freepik.com/free-photo/digital-art-with-person-looking-city-landscape_23-2151065723.jpg?t=st=1723919620~exp=1723923220~hmac=63c27ce1712bcafdfd916df48c893b0b00d2488e0c8c1523cbd7bfd8e64dff39&w=996)"
       }
     },
   },
